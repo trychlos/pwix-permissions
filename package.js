09e0b7410b89af42c62c1caf72f647902ad5fcf6
Pierre Wieser <p.wieser@trychlos.org>
2024-07-15 11:50:56 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:permissions',
-    version: '1.0.0',
+    version: '1.0.1-rc',
     summary: 'The thinnest Javascript permissions manager as a Meteor package',
     git: 'https://github.com/trychlos/pwix-permissions',
     documentation: 'README.md'
